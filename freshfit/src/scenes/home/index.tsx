74d1e8bac5e86dd27fa71189731060665fecbdb1
julien14 <juxishimwe@gmail.com>
2025-06-07 19:36:30 +0200

    part

--- freshfit/src/scenes/home/index.tsx
+++ freshfit/src/scenes/home/index.tsx
@@ -16,6 +16,7 @@ type Props = {
 const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
   return (
+    // <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0 ">
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0 ">
       {/* IMAGE AND MAIN HEADER  */}
       <motion.div
